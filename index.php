d458184604a3edee22a4fba17b5356e7839bd76c
Roland Peelen <roland@peppr.nl>
2020-03-30 17:27:47 +0200

    add author

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-
+<?php
+/*
+ * Plugin Name: Konfig
+ * Plugin URI:  https://konfig.xyz
+ * Description: Konfig enables interacting with Woocommerce Cart
+ * Author:      Roland Peelen
+ * Version:     0.1.0
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
